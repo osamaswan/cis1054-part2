81ab81fba95e80884d19a692533beb0a3b254727
osamaswan <84193033+osamaswan@users.noreply.github.com>
2021-06-17 14:02:40 +0100

    Update Menu.php

--- Menu.php
+++ Menu.php
@@ -1,1 +1,8 @@
+<?php
 
+    require_once __DIR__.'/database.php';
+    $db = new Db();    
+
+    $foods = $db -> select("SELECT food.id, food.image, food.name, food.desc FROM food order by name");
+    echo $twig->render('menu.html','foods' => $foods)
+?>
